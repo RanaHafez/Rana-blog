ba90b14919eb6eb0524b6ed88ce2f4e65c86b62a
RanaHafez <ra2017ha1999@gmail.com>
2022-02-28 06:55:21 -0800

    Update Main

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ from functools import wraps
 import os
 
 app = Flask(__name__)
-# '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
@@ -20,7 +19,6 @@ Bootstrap(app)
 
 # CONNECT TO DB
 # 'sqlite:///blog.db'
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
